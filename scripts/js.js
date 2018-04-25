8f68d1291b4b2111d9bf2fad5232c9d7cdcf7c64
Conor Guider <con.guider@gmail.com>
2018-04-25 15:31:31 +0000

    tweaks and updates to syntax

--- scripts/js.js
+++ scripts/js.js
@@ -2,4 +2,19 @@ $( document ).ready(function() {
     $(function() {
             $("#datepicker").datepicker();
         });
+        
+// window.onscroll = function() {scrollFunction()};             <---- js is within HTML --->
+
+// function scrollFunction() {
+//     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//         document.getElementById("myBtn").style.display = "block";
+//     } else {
+//         document.getElementById("myBtn").style.display = "none";
+//     }
+// }
+
+// function topFunction() {        /* exported topFunction */
+//     document.body.scrollTop = 0; // For Safari
+//     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+// }; 
 });
